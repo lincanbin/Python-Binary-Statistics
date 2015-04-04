39446f5c8308a7ffe1e6aa543dde74e7cb26e500
lincanbin <lincanbin@hotmail.com>
2015-04-04 21:48:09 +0800

    file2hex.py: Format output.

--- file2hex.py
+++ file2hex.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import struct
-for current_byte in list(open('example.txt','rb').read()):
-    print hex(struct.unpack("B",current_byte)[0]),
+import struct, os
+filename = 'example.txt'
+byte_list = list(open(filename,'rb').read())
+for key in range(0, os.path.getsize(filename)):
+    print hex(struct.unpack("B", byte_list[key])[0]).ljust(4,' '),
+    print "\n" if (key+1)%10 == 0 else "",
