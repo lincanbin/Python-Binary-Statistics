63492d32202868528a50f1fd8fe1a440c7ca1511
lincanbin <lincanbin@hotmail.com>
2015-04-05 15:15:55 +0800

    nothing

--- dir_statistics.py
+++ dir_statistics.py
@@ -4,10 +4,10 @@ import struct, os
 count_one, count_filesize = 0, 0
 def count_dir(dirname):
     for parent, dirnames, filenames in os.walk(dirname):
-        for current_dirname in dirnames:
-            count_dir(os.path.join(parent, current_dirname))  #Recursive
         for current_filename in filenames:
             count_file(os.path.join(parent,current_filename)) #Count the number of 1 and 0 in current binary file. 
+        for current_dirname in dirnames:
+            count_dir(os.path.join(parent, current_dirname))  #Recursive
 def count_file(filename):
     global count_one, count_filesize
     for current_byte in list(open(filename, 'rb').read()):
@@ -15,5 +15,5 @@ def count_file(filename):
     count_filesize += os.path.getsize(filename)
     print filename + '\nSize: ' + str(os.path.getsize(filename) * 8) + ' Bits\n'
 if __name__ == '__main__':
-    count_dir('Z:\\test')
+    count_dir('C:\\WebSite\\php\\extras')
     print '\n\n\nResult: \n\nSize: ' + str(count_filesize*8) + ' Bits\nOne:  ' + str(count_one) + ' times\nZero: ' + str(count_filesize * 8 - count_one) + ' times\nOne/Zero: ' + str(float(count_one) / float(count_filesize * 8 - count_one))
